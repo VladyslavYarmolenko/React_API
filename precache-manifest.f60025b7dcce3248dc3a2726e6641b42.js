9536ff1c03ec836e56d6a29acbe2280c6d15dc7f
Your Name <vladedumail@gmail.com>
2020-09-06 22:10:46 +0300

    added build files

---
+++ precache-manifest.f60025b7dcce3248dc3a2726e6641b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3e9cd66f4a6a07b0402785873774d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbfb11a45e515875448d",
+    "url": "/static/css/main.afc84ab4.chunk.css"
+  },
+  {
+    "revision": "a18eeb93b17c1cad6d3d",
+    "url": "/static/js/2.ab7131a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ab7131a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbfb11a45e515875448d",
+    "url": "/static/js/main.ac8e71df.chunk.js"
+  },
+  {
+    "revision": "60334080a3128d4a2926",
+    "url": "/static/js/runtime-main.d32eecf1.js"
+  }
+]);
